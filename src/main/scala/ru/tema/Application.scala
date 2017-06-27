fef8196b87e864b1315aa70b65b1d44effe00a37
Akhtyam Sakaev <ahtyam.s@livetex.ru>
2017-06-28 01:14:46 +0300

    stats wip

--- src/main/scala/ru/tema/Application.scala
+++ src/main/scala/ru/tema/Application.scala
@@ -1,37 +1,46 @@
 package ru.tema
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
-import ru.tema.api.WeatherApi
-import ru.tema.darksky.{ DarkSkyClient, Location, Observation }
+import ru.tema.api.{ WeatherApi, WeatherApiResponse }
+import ru.tema.darksky.{ DarkSkyClient, Location }
 
-import scala.concurrent.duration.Duration
-import scala.concurrent.{ Await, Future }
+import scala.concurrent.Future
 
 
 object Application extends App {
   implicit val system = ActorSystem("my-system")
   implicit val materializer = ActorMaterializer()
 
-  val apiKey = ""
+  val apiKey = "90ed629753ab611b3c77e053ba8584df"
   val darkSkyClient = new DarkSkyClient(apiKey)
   val weatherApi = new WeatherApi(darkSkyClient)
 
-  def getObservations(lat: Double, lon: Double, fromTime: Long, days: Int): Future[Seq[Observation]] = {
-    weatherApi.getHistory(Location(lat, lon), fromTime, days)
+  def getObservations(lat: Double, lon: Double, date: String, days: Int): Future[WeatherApiResponse] = {
+    val parsedDate = time(date)
+    println(s"parsed date: $parsedDate")
+    weatherApi.getHistory(Location(lat, lon), parsedDate, days)
+  }
+
+  private def time(str: String): LocalDate = {
+    val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+    LocalDate.parse(str, formatter)
   }
 
   val route: Route =
     get {
       path("gethistory") {
-        parameters('lat.as[Double], 'lon.as[Double], 'fromtime.as[Long], 'days.as[Int]) { (lat, lon, fromTime, days) =>
-          println(s"lan: $lat, lon: $lon, from: $fromTime, days: $days")
+        parameters('lat.as[Double], 'lon.as[Double], 'date.as[String], 'days.as[Int]) { (lat, lon, date, days) =>
+          println(s"lan: $lat, lon: $lon, date: $date, days: $days")
 
-          val observations: Future[Seq[Observation]] = getObservations(lat, lon, fromTime, days)
+          val observations: Future[WeatherApiResponse] = getObservations(lat, lon, date, days)
 
           onSuccess(observations) { obs =>
             complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, obs.toString))
@@ -40,6 +49,5 @@ object Application extends App {
       }
     }
 
-  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-  Await.result(bindingFuture, Duration.Inf)
+  Http().bindAndHandle(route, "localhost", 8080)
 }
--- src/main/scala/ru/tema/api/WeatherApi.scala
+++ src/main/scala/ru/tema/api/WeatherApi.scala
@@ -1,22 +1,82 @@
 package ru.tema.api
 
+import java.time.{ LocalDate, ZoneOffset, ZonedDateTime }
+
 import ru.tema.darksky.{ DarkSkyClient, Location, Observation }
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
+
 // TODO: IO execution context. not global
+
+
+case class Stats(standardDeviation: Double, median: Double, min: Double, max: Double)
+case class DetailedStats(temp: Stats, humidity: Stats, windStrength: Stats, windBearing: Stats)
+case class DayStats(twentyFourHours: DetailedStats, day: DetailedStats, night: DetailedStats)
+case class Day(observation: Seq[Observation], dayStats: DayStats)
+
+case class WeatherApiResponse(days: Seq[Day], overallStats: DetailedStats)
+
 
 // draft
 class WeatherApi(darkSkyClient: DarkSkyClient) {
 
-  def getHistory(location: Location, fromTime: Long, days: Int): Future[Seq[Observation]] = {
-    val timestamps = daysList(fromTime, days)
+  def getHistory(location: Location, fromTime: LocalDate, days: Int): Future[WeatherApiResponse] = {
+    val endOfTheDayTimestamp = endOfTheDay(fromTime)
+    println(s"endOfTheDay: $endOfTheDayTimestamp")
+
+    val timestamps = timestampsSeq(endOfTheDayTimestamp, days)
+    println(s"timestamps: $timestamps")
+
     val futures = timestamps.map(ts => darkSkyClient.history(location, ts).map(_.hourly.data))
-    Future.sequence(futures).map(_.flatten) // TODO: sort reversed?
+    for {
+      daysObservations <- Future.sequence(futures)
+    } yield {
+      val daysResult = daysObservations.map(obs => Day(obs, dayStats(obs)))
+      val allObservations = daysObservations.flatten
+      WeatherApiResponse(daysResult, detailedStats(allObservations))
+    }
   }
 
-  private def daysList(fromTime: Long, days: Int): Seq[Long] = {
+
+  private def stats(series: Seq[Double]): Stats = {
+    Stats(standardDeviation(series), median(series), min(series), max(series))
+  }
+
+  private def detailedStats(observations: Seq[Observation]): DetailedStats = {
+    val temp = observations.map(_.temperature)
+    val humidity = observations.map(_.humidity)
+    val windSpeed = observations.map(_.windSpeed)
+    val windBearing = observations.map(_.windBearing)
+    DetailedStats(stats(temp), stats(humidity), stats(windSpeed), stats(windBearing))
+  }
+
+  private def dayStats(observations: Seq[Observation]): DayStats = {
+    val half = observations.length / 2
+    val (dayz, nightz) = observations.splitAt(half) // TODO: day/night groupBy hours (22 - 06)?
+    DayStats(detailedStats(observations), detailedStats(dayz), detailedStats(nightz))
+  }
+
+
+  // math
+  // TODO: implement
+  def standardDeviation(series: Seq[Double]): Double = 0
+  def median(series: Seq[Double]): Double = 0
+  def min(series: Seq[Double]): Double = 0
+  def max(series: Seq[Double]): Double = 0
+
+
+  // time
+  // TODO: Date is deprecated
+  private def endOfTheDay(date: LocalDate): Long = {
+    ZonedDateTime
+      .of(date.getYear, date.getMonthValue, date.getDayOfMonth, 23, 59, 59, 0, ZoneOffset.UTC)
+      .toEpochSecond
+  }
+
+  // TODO: fix and test
+  private def timestampsSeq(fromTime: Long, days: Int): Seq[Long] = {
     val day = 24 * 60 * 60 * 1000
     0 until days map (x => fromTime - x * day)
   }
