91d6130560b0079f4a6ed034bf228cb0518f2f11
Akhtyam Sakaev <ahtyam.s@livetex.ru>
2017-07-13 00:41:27 +0300

    PublicApi

--- src/main/scala/ru/tema/weather/PublicApi.scala
+++ src/main/scala/ru/tema/weather/PublicApi.scala
@@ -1,17 +1,23 @@
-package ru.tema.api
+package ru.tema.weather
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 import ru.tema.darksky.Location
-import ru.tema.weather.{ HistoryResponse, WeatherService }
+import ru.tema.repository.CitiesRepo
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class RestApi(weatherService: WeatherService) {
 
+class PublicApi(weatherService: WeatherService, citiesRepo: CitiesRepo) {
   private val inputPattern = "dd-MM-yyyy"
   private val inputFormatter = DateTimeFormatter.ofPattern(inputPattern)
+
+  def locations(cities: Seq[String]): Future[Seq[Location]] = {
+    val futures = cities.map(citiesRepo.location)
+    Future.sequence(futures).map(_.flatten)
+  }
 
   def history(lat: Double, lon: Double, date: String, days: Int): Future[HistoryResponse] = {
     val localDate = parseTime(date)
