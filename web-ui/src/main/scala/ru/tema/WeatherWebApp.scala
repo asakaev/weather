e99b6565a598d0d0d7b3890a095854cb4093a3f0
Akhtyam Sakaev <ahtyam.s@livetex.ru>
2017-07-16 17:46:35 +0300

    ui hello world

--- web-ui/src/main/scala/ru/tema/WeatherWebApp.scala
+++ web-ui/src/main/scala/ru/tema/WeatherWebApp.scala
@@ -1,8 +1,27 @@
 package ru.tema
 
+import org.scalajs.dom
+import dom.document
+
+import scala.scalajs.js.annotation.JSExportTopLevel
+
 
 object WeatherWebApp {
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
+  }
+
+  @JSExportTopLevel("addClickedMessage")
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    println("WeatherWebApp logs")
+    appendPar(document.body, "WeatherWebApp init")
   }
 }
