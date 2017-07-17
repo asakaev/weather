e8549a5cfaa5e6b5eefe1da633698433fd622817
Akhtyam Sakaev <ahtyam.s@livetex.ru>
2017-07-17 21:56:57 +0300

    routes upd

--- src/main/scala/ru/tema/weather/WebServer.scala
+++ src/main/scala/ru/tema/weather/WebServer.scala
@@ -11,38 +11,49 @@ import org.json4s._
 import org.json4s.native.Serialization._
 import ru.tema.darksky.Location
 
+import scala.util.{ Failure, Success }
+
 
 class WebServer(host: String, port: Int, publicApi: PublicApi) {
   implicit val system = ActorSystem("my-system")
   implicit val materializer = ActorMaterializer()
   implicit val formats = DefaultFormats
 
-  val route: Route =
-    get {
-      path("locations") {
-        parameters('city.*) { (cities) =>
-          val citiesSeq = cities.toSeq
-          println(s"cities: $citiesSeq")
-          val locationsFuture = publicApi.locations(citiesSeq)
-          onSuccess(locationsFuture) { locations =>
-            val jsonString = write(locations)
-            val httpEntity = HttpEntity(ContentTypes.`application/json`, jsonString)
-            complete(httpEntity)
-          }
+
+  val locations: Route =
+    path("locations") {
+      parameters('city.*) { (cities) =>
+        val citiesSeq = cities.toSeq
+        println(s"cities: $citiesSeq")
+        val locationsFuture = publicApi.locations(citiesSeq)
+        onSuccess(locationsFuture) { locations =>
+          val jsonString = write(locations)
+          val httpEntity = HttpEntity(ContentTypes.`application/json`, jsonString)
+          complete(httpEntity)
         }
-      } ~
-        path("history") {
-          parameters('lat.as[Double], 'lon.as[Double], 'date.as[String], 'days.as[Int]) { (lat, lon, date, days) =>
-            println(s"lat: $lat, lon: $lon, date: $date, days: $days")
-            val historyFuture = publicApi.history(Location(lat, lon), date, days)
-            onSuccess(historyFuture) { response =>
-              // TODO: JSON response
-              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, response.toString))
-            }
-          }
-        }
+      }
     }
 
+  val history: Route =
+    path("history") {
+      parameters('lat.as[Double], 'lon.as[Double], 'date.as[String], 'days.as[Int]) { (lat, lon, date, days) =>
+        println(s"lat: $lat, lon: $lon, date: $date, days: $days")
+        val historyFuture = publicApi.history(Location(lat, lon), date, days)
+        onComplete(historyFuture) {
+          case Success(historyResponse) =>
+            val jsonString = write(historyResponse)
+            val httpEntity = HttpEntity(ContentTypes.`application/json`, jsonString)
+            complete(httpEntity)
+          case Failure(e) =>
+            println(s"Internal error: $e")
+            // TODO: status 500 error
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "FAIL"))
+          //                failWith(new Exception("wtf"))
+        }
+      }
+    }
+
+  val route: Route = get { locations ~ history }
   val corsRoute: Route = cors() { route }
 
   Http().bindAndHandle(corsRoute, host, port)
