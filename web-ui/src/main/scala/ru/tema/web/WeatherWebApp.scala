29a8e46e7cbea4ca2efb39d96e3e1e168d45bcb5
Akhtyam Sakaev <ahtyam.s@livetex.ru>
2017-07-17 12:50:48 +0300

    minor

--- web-ui/src/main/scala/ru/tema/web/WeatherWebApp.scala
+++ web-ui/src/main/scala/ru/tema/web/WeatherWebApp.scala
@@ -20,8 +20,8 @@ object WeatherWebApp {
   def setupUI(): Unit = {
     jQuery("body").append(s"<p>Cities: ${cities.mkString(", ")}</p>")
     jQuery("body").append(s"<p>Date: $localDate</p>")
-    jQuery("body").append("""<button id="locations-button" type="button">Locations</button>""")
-    jQuery("#locations-button").click(() => onLocationsBtn())
+    jQuery("body").append("""<button id="locations-btn" type="button">Locations</button>""")
+    jQuery("#locations-btn").click(() => onLocationsBtn())
   }
 
   def onLocationsBtn(): Unit = {
