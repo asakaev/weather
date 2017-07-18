4d930e416263a47321df7045dcb375dc77328947
Akhtyam Sakaev <ahtyam.s@livetex.ru>
2017-07-19 01:43:59 +0300

    d3 line

--- web-ui/src/main/scala/ru/tema/web/WeatherWebApp.scala
+++ web-ui/src/main/scala/ru/tema/web/WeatherWebApp.scala
@@ -3,22 +3,16 @@ package ru.tema.web
 import java.time.LocalDate
 
 import org.scalajs.jquery.jQuery
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.scalajs.js.{ Date, Dictionary }
 import org.singlespaced.d3js.Ops._
 import org.singlespaced.d3js.d3
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js
-//import scala.scalajs.js._
-import org.scalajs.dom
+import scala.scalajs.js.JSConverters._
+import scala.util.{ Failure, Success }
+
 
 object WeatherWebApp {
-
-  trait Data {
-    var date: Date
-    var close: Double
-  }
 
   private val endpoint = "http://localhost:8080"
   private val weatherApiClient = new WeatherApiClient(endpoint)
@@ -33,7 +27,7 @@ object WeatherWebApp {
     jQuery("body").append(s"<p>Date: $localDate</p>")
     jQuery("body").append("""<button id="locations-btn" type="button">Locations</button>""")
     jQuery("#locations-btn").click(() => onLocationsBtn())
-    zzz()
+    drawD3()
   }
 
   def handleResults(results: Seq[HistoryResponse]) = {
@@ -63,7 +57,7 @@ object WeatherWebApp {
     }
   }
 
-  def zzz(): Unit = {
+  def drawD3(): Unit = {
 
     object margin {
       val top = 30
@@ -75,7 +69,7 @@ object WeatherWebApp {
     val width = 600 - margin.left - margin.right
     val height = 270 - margin.top - margin.bottom
 
-    val parseDate = d3.time.format("%d-%b-%y").parse(_)
+//    val parseDate = d3.time.format("%d-%b-%y").parse(_)
 
     val x = d3.time.scale().range(js.Array(0, width))
     val y = d3.scale.linear().range(js.Array(height, 0))
@@ -85,9 +79,9 @@ object WeatherWebApp {
     val yAxis = d3.svg.axis().scale(y)
       .orient("left").ticks(5)
 
-    val valueline = d3.svg.line[Data]()
-      .x { (d: Data) => x(d.date) }
-      .y { (d: Data) => y(d.close) }
+    val valueline = d3.svg.line[DataPoint]()
+      .x { (d: DataPoint) => x(d.time) }
+      .y { (d: DataPoint) => y(d.temperature) }
 
     val svg = d3.select("body")
       .append("svg")
@@ -97,44 +91,60 @@ object WeatherWebApp {
       .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")")
 
-    d3.csv("data.csv", { (error: Any, rawdata: js.Array[Dictionary[String]]) =>
+    // ----------------------------------------------------------
 
-      val data: js.Array[Data] = rawdata.map { record =>
-        object d extends Data {
-          var date = parseDate(record("date"))
-          var close = record("close").toDouble
-        }
-        d
-      }
+    // TODO: from RPC
+    val cities = Seq(
+      City("Moscow", Location(55.751244,37.618423)),
+      City("Saint-Petersburg", Location(59.8944444,30.2641667))
+    )
 
-      val z1z2 = d3.extent(data.map(_.date))
-      val minx = z1z2._1
-      val maxx = z1z2._2
-      x.domain(js.Array(minx, maxx))
+    val locations = cities.map(_.location)
+    val date = LocalDate.of(2017, 6, 17)
 
-      val maxy = d3.max(data.map(_.close))
-      y.domain(js.Array(0, maxy))
+    val eventualHistoryResponses = weatherApiClient.history(locations, date, 1)
 
-      svg.append("path")
-        .attr("class", "line")
-        .datum(data)  // This is needed to reference the actual data
-        .attr("d", valueline)
+    eventualHistoryResponses.onComplete {
+      case Failure(e) => println(e)
+      case Success(historyResponses) =>
+        println("------------------")
+        println(historyResponses)
+        val history = historyResponses.head // TODO: warn
+        val dataPoints: Seq[DataPoint] = history.days.flatMap(_.dataPoints)
 
-//      svg.append("path")
-//        .attr("class", "line")
-//        .attr("d", valueline(data))
+        val dates = dataPoints.map(_.time) // .map(epoch => new js.Date(epoch))
+        println(s"dates: $dates")
+        val minX = new js.Date(dates.min)
+        val maxX = new js.Date(dates.max)
+        val datesRange = js.Array(minX, maxX)
+        println(s">>>>>>>> datesRange: $datesRange")
+        x.domain(datesRange)
 
-      svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
+        val temps = dataPoints.map(_.temperature)
+        val maxY = temps.max
+        val tempsRange = js.Array(0, maxY)
+        println(s">>>>>>>> tempsRange: $tempsRange")
+        y.domain(tempsRange)
 
-      svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
 
-      ()
-    })
+        val jsDataPoints: js.Array[DataPoint] = dataPoints.toJSArray
+
+        svg.append("path")
+          .attr("class", "line")
+          .datum(jsDataPoints)  // This is needed to reference the actual data
+          .attr("d", valueline)
+
+        svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis)
+
+        svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+
+    }
+
   }
 
 
