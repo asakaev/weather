f37af1f68ae634c5530f3ef83f4704c15184b972
Akhtyam Sakaev <ahtyam.s@livetex.ru>
2017-07-22 19:24:12 +0300

    upd

--- src/main/scala/ru/tema/darksky/DarkSkyClient.scala
+++ src/main/scala/ru/tema/darksky/DarkSkyClient.scala
@@ -54,19 +54,13 @@ trait DarkSkyService {
 class DarkSkyDummy extends DarkSkyService {
   override def history(location: Location, time: LocalDateTime): Future[Response] = {
     println(s"Dummy history for $location at $time")
-    def toDataPoint(x: Int) = DataPoint(
-      unixEpochDay(x),
-      intUpTo24,
-      Random.nextLong,
-      Random.nextLong,
-      Random.nextLong
-    )
-
+    def toDataPoint(x: Int) = DataPoint(unixEpochDay(x), tempRand, rand, rand, rand)
     val dataPoints = 0 until 24 map toDataPoint
     Future.successful(Response(DataBlock(dataPoints), "Europe/Moscow"))
   }
 
-  private def intUpTo24 = Math.abs(Random.nextLong) % 24
+  private def tempRand = Math.abs(Random.nextLong) % 24
+  private def rand = Random.nextLong
   private def unixEpochDay(x: Int) = x * 24 * 60 * 60
 }
 
